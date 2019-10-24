aeea072b4d292ce4c90c7e3a7cf2fc9271c60cf5
J0ck3l <56871282+J0ck3l@users.noreply.github.com>
2019-10-24 15:25:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello to Julians World!");
     response.end("Does this have an effect?");
+    response.end("Now there should be some output")
 });
 
 const port = process.env.PORT || 1337;
