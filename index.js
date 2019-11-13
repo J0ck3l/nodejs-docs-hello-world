17f29ee99771668360d9819364b4d154ecdfcb9a
Julian Mosteller <julian.mosteller@aequitas-integration.de>
2019-11-13 15:20:12 +0100

    Changed text output in index.js.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello to Julians World! See how the world changes ;)");
+    response.end("Hey Julian");
 });
 
 const port = process.env.PORT || 1337;
