81e96978b9cba555835b1f9335d16d0663e87dd1
J0ck3l <56871282+J0ck3l@users.noreply.github.com>
2019-11-14 13:49:15 +0100

    Changed text to test ACR login

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Test mit Angabe der resource group.");
+    response.end("Nochmal test mit ACR login.");
 });
 
 const port = process.env.PORT || 1337;
