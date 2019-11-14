0685af91f8bfb00fefdac0b15782e7fd249aceba
J0ck3l <56871282+J0ck3l@users.noreply.github.com>
2019-11-14 14:48:32 +0100

    Fixed typo in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Owner Rolle für SP.);
+    response.end("Owner Rolle für SP. Inclusive Tüdelchen");
 });
 
 const port = process.env.PORT || 1337;
