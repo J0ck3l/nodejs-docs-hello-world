e37e170a51c3bd0f10b26eb60a1b09fccfac4951
J0ck3l <56871282+J0ck3l@users.noreply.github.com>
2019-11-14 09:23:20 +0100

    Changed Text message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Nun kommt der hammer Text! :O");
+    response.end("Neuer Text mit Lars am Start");
 });
 
 const port = process.env.PORT || 1337;
