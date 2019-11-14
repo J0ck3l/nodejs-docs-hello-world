eee2f96f866f76f1f5cf48945ba3d301f2d72dd5
J0ck3l <56871282+J0ck3l@users.noreply.github.com>
2019-11-14 12:12:12 +0100

    Changed message to test pushing to ACR.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Neuer Text mit Lars am Start");
+    response.end("Nun der ultimative Test mit build und push image to ACR!");
 });
 
 const port = process.env.PORT || 1337;
