a9cc70347ce73eca899f0084ca941c3e6e0a198e
J0ck3l <56871282+J0ck3l@users.noreply.github.com>
2019-11-20 16:09:41 +0100

    SP has to be owner of resource group

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Test mit Event Grid Viewer, Dockerfile Anpassung und Erstellung eines Container in Azure aus Jenkins.");
+    response.end("Test mit Event Grid Viewer, Dockerfile Anpassung und Erstellung eines Containers in Azure aus Jenkins. Service Principal ist jetzt owner der Resource Group.");
 });
 
 const port = process.env.PORT || 1337;
