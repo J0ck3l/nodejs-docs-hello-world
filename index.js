acd942a430d6b8b7b002374d1a8d707b262a02a0
J0ck3l <56871282+J0ck3l@users.noreply.github.com>
2019-11-20 14:11:17 +0100

    Test with Event Grid Viewer

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Test mit gültiger build number!");
+    response.end("Test mit Event Grid Viewer");
 });
 
 const port = process.env.PORT || 1337;
