8d7c49ed0ccd261e9331330b3123025c8f19a95c
J0ck3l <56871282+J0ck3l@users.noreply.github.com>
2019-11-20 15:33:42 +0100

    Changed Dockerfile to include starting node.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Test mit Event Grid Viewer");
+    response.end("Test mit Event Grid Viewer und Dockerfile Anpassung");
 });
 
 const port = process.env.PORT || 1337;
