d592b6526ccff7bdc5a75c7ee832e020639df16d
J0ck3l <56871282+J0ck3l@users.noreply.github.com>
2019-11-22 13:57:02 +0100

    Test: Showing tags in acr repository.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Test mit az acr list.");
+    response.end("Test: Anzeigen der Tags im Repository.");
 });
 
 const port = process.env.PORT || 1337;
