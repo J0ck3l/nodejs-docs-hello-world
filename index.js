558efac2e5b1cf299a8bd92a30c741c983db5d85
J0ck3l <56871282+J0ck3l@users.noreply.github.com>
2019-11-22 15:10:03 +0100

    Test: Try sleep 20s before pulling image

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Test: Service Principal mit Leseberechtigung auf der Subscription. Und sleep 5s.");
+    response.end("Test: Service Principal mit Leseberechtigung auf der Subscription. Und sleep 20s.");
 });
 
 const port = process.env.PORT || 1337;
