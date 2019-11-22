3e71a2e88fa3a4e1756a891398b8171d637eba52
J0ck3l <56871282+J0ck3l@users.noreply.github.com>
2019-11-22 13:49:58 +0100

    Try to list images in acr.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Test mit Container Erzeugung in Azure aus JenkinsVM.");
+    response.end("Test mit az acr list.");
 });
 
 const port = process.env.PORT || 1337;
