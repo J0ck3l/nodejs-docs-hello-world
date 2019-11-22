d3fd4a618198b4d5cea8de13be1f41eb4fa99b03
J0ck3l <56871282+J0ck3l@users.noreply.github.com>
2019-11-22 14:48:44 +0100

    Test: Service principal with read authorization.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Test: Anzeigen der Tags im Repository.");
+    response.end("Test: Service Principal mit Leseberechtigung auf der Subscription.");
 });
 
 const port = process.env.PORT || 1337;
